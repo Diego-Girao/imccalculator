50bfaf430432845be0ba602464499c34715de43f
diegogirao <diegoramiro25@gmail.com>
2023-05-27 21:33:11 +0100

    style: created style for result texts

--- src/components/ResultImc/index.js
+++ src/components/ResultImc/index.js
@@ -1,11 +1,26 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
 
 export default function ResultImc(props) {
 	return (
 		<View>
-			<Text>{props.messageResultImc}</Text>
-			<Text>{props.resultImc}</Text>
+			<Text style={styles.result}>{props.messageResultImc}</Text>
+			<Text style={styles.number}>{props.resultImc}</Text>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	result: {
+		fontSize: 16,
+		fontWeight: "bold",
+		color: "#4B0082",
+		paddingTop: 12,
+	},
+	number: {
+		fontSize: 14,
+		fontWeight: "bold",
+		color: "#4B0082",
+		paddingTop: 12,
+	},
+})
