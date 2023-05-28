35303466da0ca0687f486d040eb6580264c5af29
diegogirao <diegoramiro25@gmail.com>
2023-05-28 18:39:17 +0100

    feat: created the title style component

---
+++ src/components/Title/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+	boxTitle: {
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#f6f6f6",
+	},
+	textTitle: {
+		color: "#008080",
+		padding: 15,
+		fontSize: 24,
+		fontWeight: "bold",
+	},
+})
+
+export default styles
