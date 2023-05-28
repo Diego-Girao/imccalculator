f879e8a618ab151b239cfcdcdc6118ad979c7e4f
diegogirao <diegoramiro25@gmail.com>
2023-05-28 18:40:38 +0100

    feat: created the resultimc  style component

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+	contentImc: {
+		flex: 1,
+		marginTop: 15,
+		paddingTop: 10,
+		borderRadius: 50,
+		alignItems: "center",
+		width: "100%",
+	},
+	numberImc: {
+		fontSize: 48,
+		color: "#008080",
+		fontWeight: "bold",
+	},
+	information: {
+		fontSize: 18,
+		color: "#008080",
+		fontWeight: "bold",
+	},
+})
+
+export default styles
